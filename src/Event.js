482c07885cba01aa7b4f0fea89502abc0dffd660
Gabriel Nordeborn <gabbe.nord@gmail.com>
2024-10-25 08:15:30 +0200

    2.0.0-alpha.1 and rescript v12 support

--- src/Event.js
+++ src/Event.js
@@ -2,46 +2,46 @@
 
 
 function classify(evt) {
-  var match = typeof evt;
+  let match = typeof evt;
   switch (match) {
     case "string" :
-        return {
-                TAG: "String",
-                _0: evt
-              };
+      return {
+        TAG: "String",
+        _0: evt
+      };
     case "symbol" :
-        return {
-                TAG: "Symbol",
-                _0: evt
-              };
+      return {
+        TAG: "Symbol",
+        _0: evt
+      };
     default:
       return "Unknown";
   }
 }
 
 function eq(event1, event2) {
-  var match = typeof event1;
-  var match$1 = typeof event2;
+  let match = typeof event1;
+  let match$1 = typeof event2;
   switch (match) {
     case "string" :
-        if (match$1 === "string") {
-          return event1 === event2;
-        } else {
-          return false;
-        }
+      if (match$1 === "string") {
+        return event1 === event2;
+      } else {
+        return false;
+      }
     case "symbol" :
-        if (match$1 === "symbol") {
-          return event1 === event2;
-        } else {
-          return false;
-        }
+      if (match$1 === "symbol") {
+        return event1 === event2;
+      } else {
+        return false;
+      }
     default:
       return false;
   }
 }
 
 export {
-  classify ,
-  eq ,
+  classify,
+  eq,
 }
 /* No side effect */
--- src/Main.js
+++ src/Main.js
@@ -2,153 +2,153 @@
 
 import * as Globals$RescriptBun from "./Globals.js";
 
-var AsyncHooks;
+let AsyncHooks;
 
-var BunCrypto;
+let BunCrypto;
 
-var Types;
+let Types;
 
-var $$Bun;
+let $$Bun;
 
-var Fs;
+let Fs;
 
-var HTMLRewriter;
+let HTMLRewriter;
 
-var Os;
+let Os;
 
-var Path;
+let Path;
 
-var PerfHooks;
+let PerfHooks;
 
-var Stream;
+let Stream;
 
-var StringDecoder;
+let StringDecoder;
 
-var Test;
+let Test;
 
-var VM;
+let VM;
 
-var WorkerThreads;
+let WorkerThreads;
 
-var BufferSource = Globals$RescriptBun.BufferSource;
+let BufferSource = Globals$RescriptBun.BufferSource;
 
-var HeadersInit = Globals$RescriptBun.HeadersInit;
+let HeadersInit = Globals$RescriptBun.HeadersInit;
 
-var $$Headers = Globals$RescriptBun.$$Headers;
+let Headers = Globals$RescriptBun.Headers;
 
-var $$FormData = Globals$RescriptBun.$$FormData;
+let FormData = Globals$RescriptBun.FormData;
 
-var $$AbortSignal = Globals$RescriptBun.$$AbortSignal;
+let AbortSignal = Globals$RescriptBun.AbortSignal;
 
-var ReadableStreamDirectController = Globals$RescriptBun.ReadableStreamDirectController;
+let ReadableStreamDirectController = Globals$RescriptBun.ReadableStreamDirectController;
 
-var $$WritableStream = Globals$RescriptBun.$$WritableStream;
+let WritableStream = Globals$RescriptBun.WritableStream;
 
-var ReadableStreamGenericReader = Globals$RescriptBun.ReadableStreamGenericReader;
+let ReadableStreamGenericReader = Globals$RescriptBun.ReadableStreamGenericReader;
 
-var $$ReadableStreamDefaultReader = Globals$RescriptBun.$$ReadableStreamDefaultReader;
+let ReadableStreamDefaultReader = Globals$RescriptBun.ReadableStreamDefaultReader;
 
-var $$ReadableStream = Globals$RescriptBun.$$ReadableStream;
+let ReadableStream = Globals$RescriptBun.ReadableStream;
 
-var ReadableWritablePair = Globals$RescriptBun.ReadableWritablePair;
+let ReadableWritablePair = Globals$RescriptBun.ReadableWritablePair;
 
-var $$TransformStream = Globals$RescriptBun.$$TransformStream;
+let TransformStream = Globals$RescriptBun.TransformStream;
 
-var $$Blob = Globals$RescriptBun.$$Blob;
+let Blob = Globals$RescriptBun.Blob;
 
-var $$File = Globals$RescriptBun.$$File;
+let File = Globals$RescriptBun.File;
 
-var $$URLSearchParams = Globals$RescriptBun.$$URLSearchParams;
+let URLSearchParams = Globals$RescriptBun.URLSearchParams;
 
-var $$Response = Globals$RescriptBun.$$Response;
+let Response = Globals$RescriptBun.Response;
 
-var XMLHttpRequestBodyInit = Globals$RescriptBun.XMLHttpRequestBodyInit;
+let XMLHttpRequestBodyInit = Globals$RescriptBun.XMLHttpRequestBodyInit;
 
-var BodyInit = Globals$RescriptBun.BodyInit;
+let BodyInit = Globals$RescriptBun.BodyInit;
 
-var $$Request = Globals$RescriptBun.$$Request;
+let Request = Globals$RescriptBun.Request;
 
-var $$Crypto = Globals$RescriptBun.$$Crypto;
+let Crypto = Globals$RescriptBun.Crypto;
 
-var $$TextEncoder = Globals$RescriptBun.$$TextEncoder;
+let TextEncoder = Globals$RescriptBun.TextEncoder;
 
-var $$TextDecoder = Globals$RescriptBun.$$TextDecoder;
+let TextDecoder = Globals$RescriptBun.TextDecoder;
 
-var $$EventTarget = Globals$RescriptBun.$$EventTarget;
+let EventTarget = Globals$RescriptBun.EventTarget;
 
-var $$Event = Globals$RescriptBun.$$Event;
+let Event = Globals$RescriptBun.Event;
 
-var $$ErrorEvent = Globals$RescriptBun.$$ErrorEvent;
+let ErrorEvent = Globals$RescriptBun.ErrorEvent;
 
-var $$CloseEvent = Globals$RescriptBun.$$CloseEvent;
+let CloseEvent = Globals$RescriptBun.CloseEvent;
 
-var $$MessageEvent = Globals$RescriptBun.$$MessageEvent;
+let MessageEvent = Globals$RescriptBun.MessageEvent;
 
-var $$CustomEvent = Globals$RescriptBun.$$CustomEvent;
+let CustomEvent = Globals$RescriptBun.CustomEvent;
 
-var $$WebSocket = Globals$RescriptBun.$$WebSocket;
+let WebSocket = Globals$RescriptBun.WebSocket;
 
-var $$URL = Globals$RescriptBun.$$URL;
+let URL = Globals$RescriptBun.URL;
 
-var FetchEvent = Globals$RescriptBun.FetchEvent;
+let FetchEvent = Globals$RescriptBun.FetchEvent;
 
-var ShellOutput = Globals$RescriptBun.ShellOutput;
+let ShellOutput = Globals$RescriptBun.ShellOutput;
 
-var ShellExpression = Globals$RescriptBun.ShellExpression;
+let ShellExpression = Globals$RescriptBun.ShellExpression;
 
-var ShellPromise = Globals$RescriptBun.ShellPromise;
+let ShellPromise = Globals$RescriptBun.ShellPromise;
 
-var Shell = Globals$RescriptBun.Shell;
+let Shell = Globals$RescriptBun.Shell;
 
 export {
-  AsyncHooks ,
-  BunCrypto ,
-  Types ,
-  $$Bun ,
-  Fs ,
-  HTMLRewriter ,
-  Os ,
-  Path ,
-  PerfHooks ,
-  Stream ,
-  StringDecoder ,
-  Test ,
-  VM ,
-  WorkerThreads ,
-  BufferSource ,
-  HeadersInit ,
-  $$Headers ,
-  $$FormData ,
-  $$AbortSignal ,
-  ReadableStreamDirectController ,
-  $$WritableStream ,
-  ReadableStreamGenericReader ,
-  $$ReadableStreamDefaultReader ,
-  $$ReadableStream ,
-  ReadableWritablePair ,
-  $$TransformStream ,
-  $$Blob ,
-  $$File ,
-  $$URLSearchParams ,
-  $$Response ,
-  XMLHttpRequestBodyInit ,
-  BodyInit ,
-  $$Request ,
-  $$Crypto ,
-  $$TextEncoder ,
-  $$TextDecoder ,
-  $$EventTarget ,
-  $$Event ,
-  $$ErrorEvent ,
-  $$CloseEvent ,
-  $$MessageEvent ,
-  $$CustomEvent ,
-  $$WebSocket ,
-  $$URL ,
-  FetchEvent ,
-  ShellOutput ,
-  ShellExpression ,
-  ShellPromise ,
-  Shell ,
+  AsyncHooks,
+  BunCrypto,
+  Types,
+  $$Bun,
+  Fs,
+  HTMLRewriter,
+  Os,
+  Path,
+  PerfHooks,
+  Stream,
+  StringDecoder,
+  Test,
+  VM,
+  WorkerThreads,
+  BufferSource,
+  HeadersInit,
+  Headers,
+  FormData,
+  AbortSignal,
+  ReadableStreamDirectController,
+  WritableStream,
+  ReadableStreamGenericReader,
+  ReadableStreamDefaultReader,
+  ReadableStream,
+  ReadableWritablePair,
+  TransformStream,
+  Blob,
+  File,
+  URLSearchParams,
+  Response,
+  XMLHttpRequestBodyInit,
+  BodyInit,
+  Request,
+  Crypto,
+  TextEncoder,
+  TextDecoder,
+  EventTarget,
+  Event,
+  ErrorEvent,
+  CloseEvent,
+  MessageEvent,
+  CustomEvent,
+  WebSocket,
+  URL,
+  FetchEvent,
+  ShellOutput,
+  ShellExpression,
+  ShellPromise,
+  Shell,
 }
 /* No side effect */
